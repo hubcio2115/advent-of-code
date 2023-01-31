8b2edf94934134f114ccb3ee04f75da1a824625d
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-01-31 12:52:37 +0100

    refactored to be fully functional

--- src/zad3/zad3.rs
+++ src/zad3/zad3.rs
@@ -1,36 +1,40 @@
 use std::{collections::HashSet, fs};
 
+fn clean_common_characters(characters: Vec<&str>) -> &str {
+    return characters
+        .iter()
+        .filter(|character| *character != &"")
+        .cloned()
+        .collect::<Vec<_>>()[0];
+}
+
 fn main() {
     let input = fs::read_to_string("./src/zad3/input.txt").expect("Wrong file provided!");
     let alphanumeric = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
         .split("")
         .collect::<Vec<&str>>();
 
-    // 3.
-
+    // 1.
     let result: usize = input
         .lines()
         .map(|line| {
             let (left, right) = line.split_at(line.len() / 2);
 
-            let left_set = left.split("").collect::<HashSet<&str>>();
-            let right_set = right.split("").collect::<HashSet<&str>>();
+            let (left_set, right_set) = (
+                left.split("").collect::<HashSet<&str>>(),
+                right.split("").collect::<HashSet<&str>>(),
+            );
 
-            let mut common_characters = left_set
-                .intersection(&right_set)
-                .cloned()
-                .collect::<Vec<&str>>();
-
-            if let Some(i) = common_characters
-                .iter()
-                .position(|character| *character == "")
-            {
-                common_characters.remove(i);
-            }
+            let common_character = clean_common_characters(
+                left_set
+                    .intersection(&right_set)
+                    .cloned()
+                    .collect::<Vec<&str>>(),
+            );
 
             return alphanumeric
                 .iter()
-                .position(|letter| *letter == common_characters[0])
+                .position(|letter| letter == &common_character)
                 .unwrap();
         })
         .into_iter()
@@ -39,26 +43,32 @@ fn main() {
     println!("{result}");
 
     // 2.
-
     let result2: usize = input
         .lines()
         .collect::<Vec<_>>()
         .chunks(3)
         .map(|chunk| {
-            let first = chunk[0].split("").collect::<HashSet<_>>();
-            let second = chunk[1].split("").collect::<HashSet<_>>();
-            let third = chunk[2].split("").collect::<HashSet<_>>();
+            let (first, second, third) = (
+                chunk[0].split("").collect::<HashSet<_>>(),
+                chunk[1].split("").collect::<HashSet<_>>(),
+                chunk[2].split("").collect::<HashSet<_>>(),
+            );
 
-            let temp = first.intersection(&second).cloned().collect::<HashSet<_>>();
-            let mut common = temp.intersection(&third).collect::<Vec<_>>();
+            let common_characters = first
+                .intersection(&second)
+                .cloned()
+                .collect::<HashSet<&str>>();
 
-            if let Some(i) = common.iter().position(|character| *character == &"") {
-                common.remove(i);
-            }
+            let common_character = clean_common_characters(
+                common_characters
+                    .intersection(&third)
+                    .cloned()
+                    .collect::<Vec<_>>(),
+            );
 
             return alphanumeric
                 .iter()
-                .position(|letter| letter.eq(common[0]))
+                .position(|letter| letter == &common_character)
                 .unwrap();
         })
         .into_iter()
