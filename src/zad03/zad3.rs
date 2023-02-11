26e4322f34b3717fe88a3ef7434aff446d963502
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-02-11 13:01:21 +0100

    formatted

--- src/zad03/zad3.rs
+++ src/zad03/zad3.rs
@@ -1,78 +1,78 @@
 use std::{collections::HashSet, fs};
 
 fn clean_common_characters(characters: Vec<&str>) -> &str {
-  return characters
-    .iter()
-    .filter(|character| *character != &"")
-    .cloned()
-    .collect::<Vec<_>>()[0];
+    return characters
+        .iter()
+        .filter(|character| *character != &"")
+        .cloned()
+        .collect::<Vec<_>>()[0];
 }
 
 fn main() {
-  let input = fs::read_to_string("./src/zad03/input.txt").expect("Wrong file provided!");
-  let alphanumeric = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    .split("")
-    .collect::<Vec<&str>>();
+    let input = fs::read_to_string("./src/zad03/input.txt").expect("Wrong file provided!");
+    let alphanumeric = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        .split("")
+        .collect::<Vec<&str>>();
 
-  // 1.
-  let result: usize = input
-    .lines()
-    .map(|line| {
-      let (left, right) = line.split_at(line.len() / 2);
+    // 1.
+    let result: usize = input
+        .lines()
+        .map(|line| {
+            let (left, right) = line.split_at(line.len() / 2);
 
-      let (left_set, right_set) = (
-        left.split("").collect::<HashSet<&str>>(),
-        right.split("").collect::<HashSet<&str>>(),
-      );
+            let (left_set, right_set) = (
+                left.split("").collect::<HashSet<&str>>(),
+                right.split("").collect::<HashSet<&str>>(),
+            );
 
-      let common_character = clean_common_characters(
-        left_set
-          .intersection(&right_set)
-          .cloned()
-          .collect::<Vec<&str>>(),
-      );
+            let common_character = clean_common_characters(
+                left_set
+                    .intersection(&right_set)
+                    .cloned()
+                    .collect::<Vec<&str>>(),
+            );
 
-      return alphanumeric
-        .iter()
-        .position(|letter| letter == &common_character)
-        .unwrap();
-    })
-    .into_iter()
-    .sum();
+            return alphanumeric
+                .iter()
+                .position(|letter| letter == &common_character)
+                .unwrap();
+        })
+        .into_iter()
+        .sum();
 
-  println!("{result}");
+    println!("{result}");
 
-  // 2.
-  let result2: usize = input
-    .lines()
-    .collect::<Vec<_>>()
-    .chunks(3)
-    .map(|chunk| {
-      let (first, second, third) = (
-        chunk[0].split("").collect::<HashSet<_>>(),
-        chunk[1].split("").collect::<HashSet<_>>(),
-        chunk[2].split("").collect::<HashSet<_>>(),
-      );
+    // 2.
+    let result2: usize = input
+        .lines()
+        .collect::<Vec<_>>()
+        .chunks(3)
+        .map(|chunk| {
+            let (first, second, third) = (
+                chunk[0].split("").collect::<HashSet<_>>(),
+                chunk[1].split("").collect::<HashSet<_>>(),
+                chunk[2].split("").collect::<HashSet<_>>(),
+            );
 
-      let common_characters = first
-        .intersection(&second)
-        .cloned()
-        .collect::<HashSet<&str>>();
+            let common_characters = first
+                .intersection(&second)
+                .cloned()
+                .collect::<HashSet<&str>>();
 
-      let common_character = clean_common_characters(
-        common_characters
-          .intersection(&third)
-          .cloned()
-          .collect::<Vec<_>>(),
-      );
+            let common_character = clean_common_characters(
+                common_characters
+                    .intersection(&third)
+                    .cloned()
+                    .collect::<Vec<_>>(),
+            );
 
-      return alphanumeric
-        .iter()
-        .position(|letter| letter == &common_character)
-        .unwrap();
-    })
-    .into_iter()
-    .sum();
+            return alphanumeric
+                .iter()
+                .position(|letter| letter == &common_character)
+                .unwrap();
+        })
+        .into_iter()
+        .sum();
 
-  println!("{result2}");
+    println!("{result2}");
 }
